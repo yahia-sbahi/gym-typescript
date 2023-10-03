414f69c2e79a4a806a847cd61f4e2bcddd25a9f2
unknown <yahiasbahi1@gmail.com>
2023-10-03 21:16:45 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
          <Benefits setSelectedPage={setSelectedPage}/>
          <OurClasses setSelectedPage={setSelectedPage}/>
          <ContactUs setSelectedPage={setSelectedPage}/>
-         <Footer setSelectedPage={setSelectedPage}/>
+         <Footer/>
 
 
     </div>
